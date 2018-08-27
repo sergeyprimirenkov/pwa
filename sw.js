04edcad853be4f985d6e74418865ba6cf39d4e80
sergeyprimirenkov <serg_miren@mail.ru>
2018-08-27 20:33:51 +0500

    update files

--- sw.js
+++ sw.js
@@ -9,3 +9,13 @@ toolbox.router.get('/images/*', toolbox.cacheFirst);
 toolbox.router.get('/*', toolbox.networkFirst, {
   networkTimeoutSeconds: 5
 });
+
+this.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/'
+      ]);
+    })
+  );
+});
